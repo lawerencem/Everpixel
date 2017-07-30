8a25158c128bf0b7ad92fd3ed31b04b56c7f903c
JeremyACase <JeremyACase@gmail.com>
2017-07-30 08:53:20 -0400

    Initial

---
+++ Assets/Model/Perks/SStatModPerks/ShieldPro.cs
@@ -0,0 +1,18 @@
+﻿using Model.Characters;
+
+namespace Model.Perks
+{
+    public class ShieldPro : GenericSStadModPerk
+    {
+        public ShieldPro() : base(PerkEnum.Shield_Pro)
+        {
+
+        }
+
+        public override void TryModSStat(SecondaryStatsEnum stat, ref double value)
+        {
+            if (stat == SecondaryStatsEnum.Block)
+                value *= this.Val;
+        }
+    }
+}
