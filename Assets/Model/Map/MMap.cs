bc24cb30a44ec0743523c7f0c1a125e1b2fca899
JeremyACase <JeremyACase@gmail.com>
2017-08-07 18:51:41 -0400

    Including changes to template hex models

--- Assets/Model/Map/MMap.cs
+++ Assets/Model/Map/MMap.cs
@@ -1,175 +1,146 @@
-﻿//using Assets.Generics;
-//using Controller.Map;
-//using Generics.Hex;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using Assets.Model.Party.Enum;
+using Controller.Map;
+using System.Collections.Generic;
+using System.Linq;
+using Template.Hex;
+using Template.Other;
 
-//namespace Assets.Model.Map
-//{
-//    public class MMap
-//    {
-//        private HexMap _map;
-//        public List<TileController> TileControllers { get; set; }
-//        private Dictionary<Pair<int, int>, TileController> TileControllerMap { get; set; }
+namespace Assets.Model.Map
+{
+    public class MMap
+    {
+        private HexMap _map;
+        public List<TileController> TileControllers { get; set; }
+        private Dictionary<Pair<int, int>, TileController> TileDict { get; set; }
 
-//        public MMap(HexMap map)
-//        {
-//            this.TileControllers = new List<TileController>();
-//            this.TileControllerMap = new Dictionary<Pair<int, int>, TileController>();
-//            this._map = map;
-//        }
+        public MMap(HexMap map)
+        {
+            this.TileControllers = new List<TileController>();
+            this.TileDict = new Dictionary<Pair<int, int>, TileController>();
+            this._map = map;
+            var mTiles = new List<MTile>();
+            foreach(var tile in this._map.Tiles)
+                mTiles.Add(new MTile(tile));
+            foreach (var tile in mTiles)
+            {
+                tile.Init();
+                var controller = new TileController(tile);
+            }
+        }
 
-//        public void AddTileController(TileController tile)
-//        {
-//            this.TileControllers.Add(tile);
-//            var key = new Pair<int, int>(tile.Model.Col, tile.Model.Row);
-//            this.TileControllerMap.Add(key, tile);
-//        }
+        public void InitControllerAdjacent()
+        {
+            foreach (var tile in this.TileControllers)
+                foreach (var neighbor in tile.GetAdjacent())
+                    tile.GetAdjacent().Add(neighbor);
+        }
 
-//        public void InitControllerAdjacent()
-//        {
-//            foreach(var tile in this.TileControllers)
-//                foreach(var neighbor in tile.Model.Adjacent)
-//                    tile.Adjacent.Add(neighbor.Parent);
-//        }
+        public Path GetPath(MTile s, MTile g)
+        {
+            int nodeCtr = 0;
+            var validPaths = new List<Path>();
+            bool found = false;
+            var pathDict = new Dictionary<Pair<int, int>, Path>();
+            var openSet = new List<MTile>() { s };
+            var closedSet = new List<Pair<int, int>>();
+            var initPath = new Path();
+            initPath.AddTile(s);
+            pathDict.Add(new Pair<int, int>(s.Col, s.Row), initPath);
 
-//        public List<HexTile> GetAoETiles(HexTile t, int dist)
-//        {
-//            var tiles = new List<HexTile>();
+            // TODO: Update this with directional search after so many nodes....
+            while (openSet.Count > 0 && !found)
+            {
+                var tile = openSet.ElementAt(0);
+                foreach (var neighbor in tile.GetAdjacent())
+                {
+                    var neighborKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                    if (neighbor.Controller.Current == null)
+                    {
+                        nodeCtr++;
+                        if (!closedSet.Contains(neighborKey))
+                            openSet.Add(neighbor);
+                        var innerKey = new Pair<int, int>(tile.Col, tile.Row);
+                        var previousPath = pathDict[innerKey];
+                        var newPath = previousPath.DeepCopy();
+                        newPath.AddTile(neighbor);
+                        var newKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                        if (!pathDict.ContainsKey(newKey))
+                            pathDict.Add(newKey, newPath);
+                        else
+                        {
+                            if (newPath.Score < pathDict[newKey].Score)
+                                pathDict[newKey] = newPath;
+                        }
 
-//            var closedSet = new List<HexTile>();
-//            var probeSet = new List<HexTile>() { t };
-//            var waitingSet = new List<HexTile>() { };
+                        if (neighbor == g)
+                        {
+                            validPaths.Add(newPath);
+                            found = true;
+                        }
 
-//            for (int i = dist; i > 0; i--)
-//            {
-//                foreach (var tile in probeSet)
-//                {
-//                    foreach (var neighbor in tile.Adjacent)
-//                    {
-//                        var found = closedSet.Find(x => x.Col == neighbor.Col && x.Row == neighbor.Row);
-//                        if (found == null)
-//                        {
-//                            waitingSet.Add(neighbor);
-//                            tiles.Add(neighbor);
-//                            closedSet.Add(neighbor);
-//                        }
-//                    }
-//                }
-//                probeSet.Clear();
-//                foreach (var tile in waitingSet) { probeSet.Add(tile); }
-//                waitingSet.Clear();
-//            }
+                    }
+                    else
+                        closedSet.Add(neighborKey);
+                }
 
-//            return tiles;
-//        }
+                closedSet.Add(new Pair<int, int>(tile.Col, tile.Row));
+                openSet.Remove(tile);
+            }
 
-//        public Path GetPath(HexTile s, HexTile g)
-//        {
-//            int nodeCtr = 0;
-//            var validPaths = new List<Path>();
-//            bool found = false;
-//            var pathDict = new Dictionary<Pair<int, int>, Path>();
-//            var openSet = new List<HexTile>() { s };
-//            var closedSet = new List<Pair<int, int>>();
-//            var initPath = new Path();
-//            initPath.AddTile(s);
-//            pathDict.Add(new Pair<int, int>(s.Col, s.Row), initPath);
+            if (validPaths.Count > 0)
+            {
+                var bestPath = validPaths.OrderBy(x => x.Score).ToList()[0];
+                return bestPath;
+            }
+            else
+                return initPath;
+        }
 
-//            // TODO: Update this with directional search after so many nodes....
-//            while (openSet.Count > 0 && !found)
-//            {
-//                var tile = openSet.ElementAt(0);
-//                foreach (var neighbor in tile.Adjacent)
-//                {
-//                    var neighborKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
-//                    if (neighbor.Current == null)
-//                    {
-//                        nodeCtr++;
-//                        if (!closedSet.Contains(neighborKey))
-//                            openSet.Add(neighbor);
-//                        var innerKey = new Pair<int, int>(tile.Col, tile.Row);
-//                        var previousPath = pathDict[innerKey];
-//                        var newPath = previousPath.DeepCopy();
-//                        newPath.AddTile(neighbor);
-//                        var newKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
-//                        if (!pathDict.ContainsKey(newKey))
-//                            pathDict.Add(newKey, newPath);
-//                        else
-//                        {
-//                            if (newPath.Score < pathDict[newKey].Score)
-//                                pathDict[newKey] = newPath;
-//                        }
-                           
-//                        if (neighbor == g)
-//                        {
-//                            validPaths.Add(newPath);
-//                            found = true;
-//                        }
+        public TileController GetTileForRow(bool lParty, EStartCol col)
+        {
+            int rowInd = -1;
+            int colInd = -1;
+            if (!lParty)
+            {
+                if (col == EStartCol.Three)
+                    colInd = this._map.GetLastCol() - 1;
+                else if (col == EStartCol.Two)
+                    colInd = this._map.GetLastCol() - 2;
+                else
+                    colInd = this._map.GetLastCol() - 3;
 
-//                    }
-//                    else
-//                        closedSet.Add(neighborKey);
-//                }
+            }
+            else
+            {
+                if (col == EStartCol.Three)
+                    colInd = this._map.GetFirstCol();
+                else if (col == EStartCol.Two)
+                    colInd = this._map.GetFirstCol() + 1;
+                else
+                    colInd = this._map.GetFirstCol() + 2;
+            }
 
-//                closedSet.Add(new Pair<int, int>(tile.Col, tile.Row));
-//                openSet.Remove(tile);
-//            }
-
-//            if (validPaths.Count > 0)
-//            {
-//                var bestPath = validPaths.OrderBy(x => x.Score).ToList()[0];
-//                return bestPath;
-//            }
-//            else
-//                return initPath;
-//        }
-
-//        public TileController GetTileForRow(bool lParty, EStartCol col)
-//        {
-//            int rowInd = -1;
-//            int colInd = -1;
-//            if (!lParty)
-//            {
-//                if (col == EStartCol.Three)
-//                    colInd = this._map.GetLastCol() - 1;
-//                else if (col == EStartCol.Two)
-//                    colInd = this._map.GetLastCol() - 2;
-//                else
-//                    colInd = this._map.GetLastCol() - 3;
-
-//            }
-//            else
-//            {
-//                if (col == EStartCol.Three)
-//                    colInd = this._map.GetFirstCol();
-//                else if (col == EStartCol.Two)
-//                    colInd = this._map.GetFirstCol() + 1;
-//                else
-//                    colInd = this._map.GetFirstCol() + 2;
-//            }
-            
-//            rowInd = this._map.GetMidRow();
-//            var key = new Pair<int, int>(colInd, rowInd);
-//            for (int i = 0; !this.TileControllerMap.ContainsKey(key) || this.TileControllerMap[key].Model.Current != null; i++)
-//            {
-//                int counter = i / 2;
-//                if (i % 2 == 1) { counter *= -1; }
-//                if (rowInd + counter > this._map.GetLastCol() - 1)
-//                {
-//                    i = 0;
-//                    if (lParty) { colInd--; }
-//                    else { colInd++; }
-//                }
-//                else if (rowInd + counter < 0)
-//                {
-//                    i = 0;
-//                    if (lParty) { colInd--; }
-//                    else { colInd++; }
-//                }
-//                key = new Pair<int, int>(colInd, rowInd + counter);
-//            }
-//            return this.TileControllerMap[key];
-//        }
-//    }
-//}
+            rowInd = this._map.GetMidRow();
+            var key = new Pair<int, int>(colInd, rowInd);
+            for (int i = 0; !this.TileDict.ContainsKey(key) || this.TileDict[key].Current != null; i++)
+            {
+                int counter = i / 2;
+                if (i % 2 == 1) { counter *= -1; }
+                if (rowInd + counter > this._map.GetLastCol() - 1)
+                {
+                    i = 0;
+                    if (lParty) { colInd--; }
+                    else { colInd++; }
+                }
+                else if (rowInd + counter < 0)
+                {
+                    i = 0;
+                    if (lParty) { colInd--; }
+                    else { colInd++; }
+                }
+                key = new Pair<int, int>(colInd, rowInd + counter);
+            }
+            return this.TileDict[key];
+        }
+    }
+}
