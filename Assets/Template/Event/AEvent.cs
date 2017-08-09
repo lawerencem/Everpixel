18c230cb098c9c7be38c3397417c1cb087329665
JeremyACase <JeremyACase@gmail.com>
2017-08-09 17:04:36 -0400

    Adding template event stuff

---
+++ Assets/Template/Event/AEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Template.Event
+{
+    public abstract class AEvent<T>
+    {
+        protected T _type;
+        public T Type { get { return this._type; } }
+
+        public AEvent(T t)
+        {
+            this._type = t;
+        }
+    }
+}
---
+++ Assets/Template/Event/AEventManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Template.Event
+{
+    public abstract class AEventManager<T>
+    {
+        protected List<T> _events;
+
+        public abstract void RegisterEvent(T t);
+        public abstract void TryProcessEvent(T t);
+    }
+}
