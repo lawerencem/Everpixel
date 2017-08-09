7bec40909ee3a50d120110b5a9db23df8ae06067
JeremyACase <JeremyACase@gmail.com>
2017-08-09 17:29:37 -0400

    Initial Template Callback stuff

---
+++ Assets/Template/Callback/ICallback.cs
@@ -0,0 +1,11 @@
+﻿namespace Template.Callback
+{
+    public delegate void Callback();
+
+    public interface ICallback
+    {
+        void AddCallback(Callback callback);
+        void Callback();
+        void SetCallback(Callback callback);
+    }
+}
