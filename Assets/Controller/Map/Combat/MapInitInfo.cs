7aa3f573ea3587b7da7839c31ed27c1095d83565
JeremyACase <JeremyACase@gmail.com>
2017-08-13 10:36:22 -0400

    Adding info class

---
+++ Assets/Controller/Map/Combat/MapInitInfo.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Model.Biome.Enum;
+
+namespace Assets.Controller.Map.Combat
+{
+    public class MapInitInfo
+    {
+        public EBiome Biome { get; set; }
+        public int COLS { get; set; }
+        public int ROWS { get; set; }
+    }
+}
