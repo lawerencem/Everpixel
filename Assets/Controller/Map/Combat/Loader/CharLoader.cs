add8119eaa472871619502ca814efc127ff1566a
JeremyACase <JeremyACase@gmail.com>
2017-08-14 19:05:08 -0400

    Refactoring

--- Assets/Controller/Map/Combat/Loader/CharLoader.cs
+++ Assets/Controller/Map/Combat/Loader/CharLoader.cs
@@ -1,9 +1,22 @@
-﻿using Assets.View.Builder;
+﻿using Assets.Controller.Character;
+using Assets.Controller.Map.Tile;
+using Assets.Model.Character.Enum;
+using Assets.View;
+using Assets.View.Builder;
+using Assets.View.Equipment;
+using UnityEngine;
 
 namespace Assets.Controller.Map.Combat.Loader
 {
     public class CharLoader
     {
+        private Transform _container;
+
+        public CharLoader(Transform container)
+        {
+            this._container = container;
+        }
+
         public void Init(MapController map, MapInitInfo info)
         {
             this.InitViews(map, info);
@@ -13,8 +26,153 @@ namespace Assets.Controller.Map.Combat.Loader
         {
             var builder = new CharacterViewBuilder();
             foreach (var party in map.GetLParties())
-                foreach (var controller in party.GetChars())
-                    controller.SetView(builder.Build(controller.Model.GetParams()));
+            {
+                foreach (var c in party.GetChars())
+                {
+                    c.SetView(builder.Build(c.Model.GetParams()));
+                    var tile = map.GetMap().GetTileForRow(c.Model.LParty, c.Model.GetParams().StartRow);
+                    this.RenderChar(map, c, tile);
+                }
+            }   
+            foreach (var party in map.GetRParties())
+            {
+                foreach (var c in party.GetChars())
+                {
+                    c.SetView(builder.Build(c.Model.GetParams()));
+                    var tile = map.GetMap().GetTileForRow(c.Model.LParty, c.Model.GetParams().StartRow);
+                    this.RenderChar(map, c, tile);
+                }
+            }   
+        }
+
+        private void RenderChar(MapController m, CharController c, TileController t)
+        {
+            var sprite = c.View.Sprites[c.View.Torso];
+            var render = c.Handle.AddComponent<SpriteRenderer>();
+            c.Handle.transform.position = t.View.Center;
+            c.Handle.transform.SetParent(this._container);
+            c.Handle.name = c.View.Type.ToString() + " " + c.View.Race.ToString();
+            render.sprite = sprite;
+            render.sortingLayerName = Layers.CHAR_TORSO;
+            this.TryAttachHead(c, Layers.CHAR_HEAD, c.View.Head, t);
+            this.TryAttachDeco(c, t);
+            this.TryAttachEquipment(c, t);
+            this.TryAttachMount(c, t);
+            c.SpriteHandlerDict.Add(Layers.CHAR_TORSO, c.Handle);
+            c.SpriteHandlerDict.Add(Layers.CHAR_MAIN, c.Handle);
+            
+            if (!c.Model.LParty)
+                c.Handle.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            t.SetCurrent(c);
+            c.SetTile(t);
+        }
+
+        private void TryAttachDeco(CharController c, TileController t)
+        {
+            this.TryAttachDecoHelper(c, Layers.CHAR_FACE, c.View.Face, t);
+            this.TryAttachDecoHelper(c, Layers.CHAR_HEAD_DECO_1, c.View.HeadDeco1, t);
+            this.TryAttachDecoHelper(c, Layers.CHAR_HEAD_DECO_2, c.View.HeadDeco2, t);
+            this.TryAttachDecoHelper(c, Layers.CHAR_TORSO_DECO_1, c.View.TorsoDeco1, t);
+            this.TryAttachDecoHelper(c, Layers.CHAR_TORSO_DECO_2, c.View.TorsoDeco2, t);
+        }
+
+        private void TryAttachDecoHelper(CharController c, string sort, int spriteIndex, TileController tile)
+        {
+            if (c.Model.Type == ECharType.Humanoid && spriteIndex >= 0)
+            {
+                var spriteHandler = new GameObject();
+                var sprite = c.View.Sprites[spriteIndex];
+                var render = spriteHandler.AddComponent<SpriteRenderer>();
+                spriteHandler.transform.position = c.Handle.transform.position;
+
+                if (sort == Layers.CHAR_FACE ||
+                    sort == Layers.CHAR_HEAD_DECO_1 ||
+                    sort == Layers.CHAR_HEAD_DECO_2)
+                {
+                    spriteHandler.transform.SetParent(c.SpriteHandlerDict[Layers.CHAR_HEAD].transform);
+                }
+                else
+                {
+                    spriteHandler.transform.SetParent(c.Handle.transform);
+                }
+
+                spriteHandler.name = sort;
+                render.sprite = sprite;
+                render.sortingLayerName = sort;
+                c.SpriteHandlerDict.Add(sort, spriteHandler);
+            }
+        }
+
+        private void TryAttachEquipment(CharController c, TileController t)
+        {
+            if (c.View.Armor != null)
+                TryAttachEquipmentHelper(c, c.View.Armor, Layers.CHAR_ARMOR, t);
+            if (c.View.Helm != null)
+                TryAttachEquipmentHelper(c, c.View.Helm, Layers.CHAR_HELM, t, 0f, ViewParams.HELM_OFFSET);
+            if (c.View.LWeapon != null)
+                TryAttachEquipmentHelper(c, c.View.LWeapon, Layers.CHAR_L_WEAPON, t, ViewParams.WEAPON_OFFSET);
+            if (c.View.RWeapon != null)
+                TryAttachEquipmentHelper(c, c.View.RWeapon, Layers.CHAR_R_WEAPON, t, -ViewParams.WEAPON_OFFSET);
+        }
+
+        private void TryAttachEquipmentHelper(CharController c, VEquipment e, string sort, TileController tile, float xOffset = 0, float yOffset = 0)
+        {
+            if (e != null)
+            {
+                var spriteHandler = new GameObject();
+                var sprite = e.Sprites[e.Index];
+                var render = spriteHandler.AddComponent<SpriteRenderer>();
+                var position = c.Handle.transform.position;
+                position.x += xOffset;
+                position.y += yOffset;
+                spriteHandler.transform.position = position;
+                if (sort == Layers.CHAR_HELM)
+                    spriteHandler.transform.SetParent(c.SpriteHandlerDict[Layers.CHAR_HEAD].transform);
+                else
+                    spriteHandler.transform.SetParent(c.Handle.transform);
+                spriteHandler.name = e.Name;
+                render.sprite = sprite;
+                render.sortingLayerName = sort;
+                c.SpriteHandlerDict.Add(sort, spriteHandler);
+            }
+        }
+
+        private void TryAttachHead(CharController c, string sort, int spriteIndex, TileController tile)
+        {
+            if (c.Model.Type == ECharType.Humanoid)
+            {
+                var sprite = c.View.Sprites[spriteIndex];
+                var spriteHandler = new GameObject();
+                var render = spriteHandler.AddComponent<SpriteRenderer>();
+                spriteHandler.transform.position = c.Handle.transform.position;
+                spriteHandler.transform.SetParent(c.Handle.transform);
+                spriteHandler.name = "Character Head";
+                render.sprite = sprite;
+                render.sortingLayerName = sort;
+                c.SpriteHandlerDict.Add(sort, spriteHandler);
+            }
+        }
+
+        private void TryAttachMount(CharController c, TileController tile)
+        {
+            if (c.View.Mount != null)
+            {
+                var sprite = c.View.Mount.Sprites[0];
+                var spriteHandler = new GameObject();
+                var render = spriteHandler.AddComponent<SpriteRenderer>();
+                var position = tile.View.Center;
+                position.x += ViewParams.MOUNT_X_OFFSET;
+                position.y -= ViewParams.MOUNT_Y_OFFSET;
+                spriteHandler.transform.position = position;
+                spriteHandler.transform.SetParent(c.Handle.transform);
+                spriteHandler.name = c.View.Name + " " + c.View.Mount.Name + " Mount";
+                render.sprite = sprite;
+                render.sortingLayerName = Layers.CHAR_MOUNT;
+                c.SpriteHandlerDict.Add(Layers.CHAR_MOUNT, spriteHandler);
+                var mountOffsetPos = c.Handle.transform.position;
+                mountOffsetPos.y += ViewParams.MOUNT_Y_OFFSET;
+                c.Handle.transform.position = mountOffsetPos;
+            }
         }
     }
 }
