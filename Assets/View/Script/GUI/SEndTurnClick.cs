3d0d6a6f1ad123aba08b5c502e89e54e8919fea5
JeremyACase <JeremyACase@gmail.com>
2017-08-21 16:32:27 -0400

    New GUI Events

---
+++ Assets/View/Script/GUI/SEndTurnClick.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Model.Event.Combat;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.View.Script.GUI
+{
+    public class SEndTurnClick : SGuiButton
+    {
+        public override void OnClick()
+        {
+            if (base.IsValid())
+            {
+                var e = new EvEndTurn();
+                e.TryProcess();
+            }
+        }
+
+        public void Start()
+        {
+            var btnContainer = GameObject.FindGameObjectWithTag("EndTurnBtnTag");
+            var btn = btnContainer.GetComponent<Button>();
+            btn.onClick.AddListener(this.OnClick);
+        }
+    }
+}
---
+++ Assets/View/Script/GUI/SGuiButton.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Controller.Manager;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.View.Script.GUI
+{
+    public class SGuiButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        protected virtual bool IsValid()
+        {
+            if (GUIManager.Instance.GetInteractionLocked())
+                return false;
+            else
+                return true;
+        }
+
+        public virtual void OnClick()
+        {
+            
+        }
+
+        public virtual void OnPointerEnter(PointerEventData eventData)
+        {
+            GUIManager.Instance.SetGUILocked(true);
+        }
+
+        public virtual void OnPointerExit(PointerEventData eventData)
+        {
+            GUIManager.Instance.SetGUILocked(false);
+        }
+    }
+}
