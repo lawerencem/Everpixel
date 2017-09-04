b9d4a45d79878bfeb51b4def0028c301407011c6
JeremyACase <JeremyACase@gmail.com>
2017-09-04 14:19:43 -0400

    Cruch fatality re-implemented

--- Assets/View/Fatality/Weapon/CrushFatality.cs
+++ Assets/View/Fatality/Weapon/CrushFatality.cs
@@ -1,4 +1,10 @@
-﻿using Assets.Template.Script;
+﻿using Assets.Controller.Character;
+using Assets.Controller.GUI.Combat;
+using Assets.Model.Character.Enum;
+using Assets.Template.Script;
+using Assets.Template.Util;
+using Assets.View.Character;
+using UnityEngine;
 
 namespace Assets.View.Fatality.Weapon
 {
@@ -9,47 +15,45 @@ namespace Assets.View.Fatality.Weapon
         public override void Init()
         {
             base.Init();
-            var position = this._data.Source.Handle.transform.position;
-            position.y -= FatalityParams.ZOOM_Y_OFFSET;
-            var zoom = this._data.Source.Handle.AddComponent<SHangCallbackZoomOut>();
-            zoom.AddCallback(this.ProcessJolt);
-            zoom.Init(position, FatalityParams.ZOOM_SPEED, FatalityParams.ZOOM_FOV, FatalityParams.ZOOM_MELEE_HANG);
+            base.Start(this.ProcessJolt);
+        }
+
+        private void ProcessCrush(object o)
+        {
+            foreach (var hit in this._data.FatalHits)
+            {
+                if (hit.Data.Target.Current != null &&
+                    hit.Data.Target.Current.GetType().Equals(typeof(CharController)))
+                {
+                    var tgt = this._data.Target.Current as CharController;
+                    var path = StringUtil.PathBuilder(
+                        CombatGUIParams.EFFECTS_PATH,
+                        CombatGUIParams.CRUSH_FATALITY,
+                        CombatGUIParams.PARTICLES_EXTENSION);
+                    var position = tgt.Handle.transform.position;
+                    var prefab = Resources.Load(path);
+                    var particles = GameObject.Instantiate(prefab) as GameObject;
+                    particles.transform.position = position;
+                    particles.name = CombatGUIParams.CRUSH_FATALITY + " Particles";
+                    var lifetime = particles.AddComponent<SDestroyByLifetime>();
+                    lifetime.lifetime = 5f;
+                    VCharUtil.Instance.ProcessDeadChar(tgt);
+                    this.CallbackHandler(this);
+                    hit.CallbackHandler(this);
+                }
+            }
         }
 
         private void ProcessJolt(object o)
         {
-
+            var pos = Vector3.Lerp(
+                this._data.Source.Handle.transform.position,
+                this._data.Target.Handle.transform.position,
+                FatalityParams.FATALITY_LERP);
+            var boomerang = this._data.Source.Handle.AddComponent<SBoomerang>();
+            boomerang.AddCallback(this.ProcessCrush);
+            boomerang.AddDoneCallback(this.AddBob);
+            boomerang.Init(this._data.Source.Handle, pos, FatalityParams.FATALITY_ATTACK_SPEED);
         }
-
-        //protected override void ProcessFatality()
-        //{
-        //    var fatalityScript = this._event.EventController.Source.Handle.AddComponent<BoomerangFatalityScript>();
-        //    var position = Vector3.Lerp(this._event.EventController.Source.Handle.transform.position, this._event.EventController.Target.transform.position, 0.55f);
-        //    fatalityScript.CallBackThree = base.Done;
-        //    fatalityScript.Init(this._event.EventController.Source.Handle, position, 0.75f, ProcessCrushFatality);
-        //}
-
-        //private void ProcessCrushFatality()
-        //{
-        //    foreach (var hit in this._event.FatalityHits)
-        //    {
-        //        this._parent.ProcessSplatter(5, this._event.EventController.TargetCharController.CurrentTile);
-        //        var path = StringUtil.PathBuilder(
-        //            CMapGUIControllerParams.EFFECTS_PATH,
-        //            CMapGUIControllerParams.CRUSH_FATALITY,
-        //            CMapGUIControllerParams.PARTICLES_EXTENSION);
-        //        var position = this._event.EventController.TargetCharController.transform.position;
-        //        var prefab = Resources.Load(path);
-        //        var particles = GameObject.Instantiate(prefab) as GameObject;
-        //        particles.transform.position = position;
-        //        particles.name = CMapGUIControllerParams.CRUSH_FATALITY + " Particles";
-        //        var lifetime = particles.AddComponent<DestroyByLifetime>();
-        //        lifetime.lifetime = 5f;
-        //        this._parent.ProcessCharacterKilled(hit.Target);
-        //        hit.Done();
-        //    }
-        //    this._event.AttackFXDone();
-        //    base.ProcessFatalityView();
-        //}
     }
 }
