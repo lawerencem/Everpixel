c1aab567d2aa8bf02b305ef794b2331d20f32652
JeremyACase <JeremyACase@gmail.com>
2017-09-20 19:20:15 -0400

    Adding IQueueCallback interface

---
+++ Assets/Template/CB/IQueueCallback.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Template.CB
+{
+    public delegate void QueueCallback(object o);
+
+    public interface IQueueCallback
+    {
+        void AddQueueCallback(int priority, QueueCallback callback);
+        void DoQueueCallbacks();
+    }
+}
