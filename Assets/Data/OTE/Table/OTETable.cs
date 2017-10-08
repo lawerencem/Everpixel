fa627860da74826f5f2d10ad28b7a1683d19a051
JeremyACase <JeremyACase@gmail.com>
2017-10-08 10:13:52 -0400

    Adding initial OTE implementation

---
+++ Assets/Data/OTE/Table/OTETable.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Model.OTE;
+using Assets.Template.Other;
+using System.Collections.Generic;
+
+namespace Assets.Data.OTE.Table
+{
+    public class OTETable : ASingleton<OTETable>
+    {
+        public Dictionary<EOTE, OTEParams> Table;
+        public OTETable()
+        {
+            Table = new Dictionary<EOTE, OTEParams>();
+        }
+    }
+}
---
+++ Assets/Model/OTE/OTEParams.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Model.Ability.Enum;
+
+namespace Assets.Model.OTE
+{
+    public class OTEParams
+    {
+        public int Dur { get; set; }
+        public EResistType Resist {get;set;}
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
