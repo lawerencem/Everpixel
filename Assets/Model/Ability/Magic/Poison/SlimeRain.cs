7ad86cbbe217df9c75790f274cda7e831fac3151
JeremyACase <JeremyACase@gmail.com>
2017-10-15 09:17:04 -0400

    Initial commit of poison abilities

---
+++ Assets/Model/Ability/Magic/Poison/SlimeRain.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Model.Ability.Enum;
+using Assets.Model.Combat.Hit;
+
+namespace Assets.Model.Ability.Magic.Poison
+{
+    public class SlimeRain : MAbility
+    {
+        public SlimeRain() : base(EAbility.Slime_Rain) { }
+
+        public override void Predict(MHit hit)
+        {
+            //base.p(hit);
+        }
+
+        public override void Process(MHit hit)
+        {
+            //base.ProcessHitBullet(hit);
+        }
+
+        public override bool IsValidActionEvent(AbilityArgs arg)
+        {
+            return true;
+        }
+    }
+}
