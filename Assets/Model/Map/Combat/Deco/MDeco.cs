d4f265869aac4e470a8686f3242e649b880bba61
JeremyACase <JeremyACase@gmail.com>
2017-11-25 11:28:42 -0500

    Initial commit

---
+++ Assets/Model/Map/Combat/Deco/MDeco.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Model.Map.Combat.Tile;
+
+namespace Assets.Model.Map.Combat.Deco
+{
+    public class MDecoData
+    {
+
+    }
+
+    public class MDeco
+    {
+        private ETileDeco _type;
+        private MDecoData _data;
+
+        public MDeco(ETileDeco type)
+        {
+            this._type = type;
+        }
+
+        public void SetData(MDecoData data) { this._data = data; }
+    }
+}
