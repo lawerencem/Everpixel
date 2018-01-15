26e963e234d8e612b6530436e3e0c1eb967eb86a
JeremyACase <JeremyACase@gmail.com>
2018-01-15 16:02:58 -0500

    Saving changes to new path search (TODO)

--- Assets/Model/Map/Combat/MMap.cs
+++ Assets/Model/Map/Combat/MMap.cs
@@ -45,9 +45,8 @@ namespace Assets.Model.Map.Combat
                     tile.GetAdjacent().Add(neighbor);
         }
 
-        public Path GetPath(MTile s, MTile g)
+        public Path GetPathViaBruteForce(MTile s, MTile g)
         {
-            int nodeCtr = 0;
             var validPaths = new List<Path>();
             bool found = false;
             var pathDict = new Dictionary<Pair<int, int>, Path>();
@@ -57,7 +56,6 @@ namespace Assets.Model.Map.Combat
             initPath.AddTile(s);
             pathDict.Add(new Pair<int, int>(s.Col, s.Row), initPath);
 
-            // TODO: Update this with directional search after so many nodes....
             while (openSet.Count > 0 && !found)
             {
                 var tile = openSet.ElementAt(0);
@@ -66,7 +64,6 @@ namespace Assets.Model.Map.Combat
                     var neighborKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
                     if (neighbor.Controller.Current == null)
                     {
-                        nodeCtr++;
                         if (!closedSet.Contains(neighborKey))
                             openSet.Add(neighbor);
                         var innerKey = new Pair<int, int>(tile.Col, tile.Row);
@@ -92,7 +89,6 @@ namespace Assets.Model.Map.Combat
                     else
                         closedSet.Add(neighborKey);
                 }
-
                 closedSet.Add(new Pair<int, int>(tile.Col, tile.Row));
                 openSet.Remove(tile);
             }
@@ -104,6 +100,115 @@ namespace Assets.Model.Map.Combat
             }
             else
                 return initPath;
+        }
+
+        public Path GetPath(MTile s, MTile g)
+        {
+            var goalOpenSet = new List<MTile>() { g };
+            var goalClosedSet = new List<Pair<int, int>>();
+            var goalInitPath = new Path();
+            var goalPaths = new List<Path>();
+            var goalPathDict = new Dictionary<Pair<int, int>, Path>();
+            goalPathDict.Add(new Pair<int, int>(g.Col, g.Row), goalInitPath);
+
+            var sourceOpenSet = new List<MTile>() { s };
+            var sourceClosedSet = new List<Pair<int, int>>();
+            var sourceInitPath = new Path();
+            var sourcePaths = new List<Path>();
+            var sourcePathDict = new Dictionary<Pair<int, int>, Path>();
+            sourcePathDict.Add(new Pair<int, int>(s.Col, s.Row), sourceInitPath);
+
+            var validPaths = new List<Path>();
+
+            while (sourceOpenSet.Count > 0 && goalOpenSet.Count > 0)
+            {
+                var goalTile = goalOpenSet.ElementAt(0);
+                foreach (var neighbor in goalTile.GetAdjacent())
+                {
+                    var neighborKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                    if (neighbor.Controller.Current == null)
+                    {
+                        if (!goalClosedSet.Contains(neighborKey))
+                            goalOpenSet.Add(neighbor);
+                        var innerKey = new Pair<int, int>(goalTile.Col, goalTile.Row);
+                        var previousPath = goalPathDict[innerKey];
+                        var newPath = previousPath.DeepCopy();
+                        newPath.AddTile(neighbor);
+                        var newKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                        if (!goalPathDict.ContainsKey(newKey))
+                            goalPathDict.Add(newKey, newPath);
+                        else
+                        {
+                            if (newPath.Score < goalPathDict[newKey].Score)
+                                goalPathDict[newKey] = newPath;
+                        }
+                        goalPaths.Add(newPath);
+                    }
+                    else
+                        goalClosedSet.Add(neighborKey);
+                }
+                goalClosedSet.Add(new Pair<int, int>(goalTile.Col, goalTile.Row));
+                goalOpenSet.Remove(goalTile);
+
+                var sourceTile = sourceOpenSet.ElementAt(0);
+
+                foreach (var neighbor in sourceTile.GetAdjacent())
+                {
+                    var neighborKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                    if (neighbor.Controller.Current == null)
+                    {
+                        if (!sourceClosedSet.Contains(neighborKey))
+                            sourceOpenSet.Add(neighbor);
+                        var innerKey = new Pair<int, int>(sourceTile.Col, sourceTile.Row);
+                        var previousPath = sourcePathDict[innerKey];
+                        var newPath = previousPath.DeepCopy();
+                        newPath.AddTile(neighbor);
+                        var newKey = new Pair<int, int>(neighbor.Col, neighbor.Row);
+                        if (!sourcePathDict.ContainsKey(newKey))
+                            sourcePathDict.Add(newKey, newPath);
+                        else
+                        {
+                            if (newPath.Score < sourcePathDict[newKey].Score)
+                                sourcePathDict[newKey] = newPath;
+                        }
+                        sourcePaths.Add(newPath);
+                    }
+                    else
+                        sourceClosedSet.Add(neighborKey);
+                }
+                sourceClosedSet.Add(new Pair<int, int>(sourceTile.Col, sourceTile.Row));
+                sourceOpenSet.Remove(sourceTile);
+
+                if (goalPaths.Count > 0 && sourcePaths.Count > 0)
+                {
+                    foreach(var sourcePath in sourcePaths)
+                    {
+                        foreach(var goalPath in goalPaths)
+                        {
+                            if (sourcePath.GetTiles()[sourcePath.GetTiles().Count - 1].Equals(
+                                goalPath.GetTiles()[goalPath.GetTiles().Count - 1]))
+                            {
+                                var valid = new Path();
+                                foreach (var tile in sourcePath.GetTiles())
+                                    valid.AddTile(tile);
+                                goalPath.GetTiles().Reverse();
+                                for (int i = 1; i < goalPath.GetTiles().Count; i++)
+                                    valid.AddTile(goalPath.GetTiles()[i]);
+                                valid.AddTile(g);
+                                validPaths.Add(valid);
+                            }
+                        }
+                    }
+                    goalPaths.Clear();
+                    sourcePaths.Clear();
+                }
+                if (validPaths.Count > 0)
+                {
+                    var bestPath = validPaths.OrderBy(x => x.Score).ToList()[0];
+                    return bestPath;
+                }
+            }
+            return null;
         }
 
         public CTile GetTileForRow(bool lParty, EStartCol col)
@@ -118,7 +223,6 @@ namespace Assets.Model.Map.Combat
                     colInd = this._map.GetLastCol() - 2;
                 else
                     colInd = this._map.GetLastCol() - 3;
-
             }
             else
             {
