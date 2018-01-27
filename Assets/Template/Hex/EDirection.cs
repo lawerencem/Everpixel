b9cb72cbe6fdba5e9a9d9038116ba0af9aa96d14
JeremyACase <JeremyACase@gmail.com>
2018-01-27 15:40:23 -0500

    Initial commit

---
+++ Assets/Template/Hex/EDirection.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Template.Util;
+
+namespace Assets.Template.Hex
+{
+    public enum EDirection
+    {
+        N,
+        NE,
+        SE,
+        S,
+        SW,
+        NW
+    }
+
+    public class EDirectionUtil
+    {
+        public static EDirection GetRandomDirection()
+        {
+            int roll = RNG.Instance.Next(0, 5);
+            if (roll == 0)
+                return EDirection.N;
+            else if (roll == 1)
+                return EDirection.NE;
+            else if (roll == 2)
+                return EDirection.SE;
+            else if (roll == 3)
+                return EDirection.S;
+            else if (roll == 4)
+                return EDirection.SW;
+            else
+                return EDirection.NW;
+        }
+    }
+}
---
+++ Assets/Template/Hex/HexGeography.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Template.Hex
+{
+    public class HexGeography
+    {
+        public List<IHex> GetTiles(EDirection direction, int dist, IHex source)
+        {
+            switch(direction)
+            {
+                case (EDirection.N): { return this.GetNTiles(dist, source); }
+                case (EDirection.NE): { return this.GetNETiles(dist, source); }
+                case (EDirection.SE): { return this.GetSETiles(dist, source); }
+                case (EDirection.S): { return this.GetSTiles(dist, source); }
+                case (EDirection.SW): { return this.GetSWTiles(dist, source); }
+                case (EDirection.NW): { return this.GetNWTiles(dist, source); }
+                default: return null;
+            }
+        }
+
+        private List<IHex> GetNTiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for(int i = 0; i < dist; i++)
+            {
+                if (source.GetN() != null)
+                {
+                    current = source.GetN();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+
+        private List<IHex> GetNETiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for (int i = 0; i < dist; i++)
+            {
+                if (source.GetNE() != null)
+                {
+                    current = source.GetN();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+
+        private List<IHex> GetSETiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for (int i = 0; i < dist; i++)
+            {
+                if (source.GetSE() != null)
+                {
+                    current = source.GetSE();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+
+        private List<IHex> GetSTiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for (int i = 0; i < dist; i++)
+            {
+                if (source.GetS() != null)
+                {
+                    current = source.GetS();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+
+        private List<IHex> GetSWTiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for (int i = 0; i < dist; i++)
+            {
+                if (source.GetSW() != null)
+                {
+                    current = source.GetSW();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+
+        private List<IHex> GetNWTiles(int dist, IHex source)
+        {
+            var tiles = new List<IHex>() { source };
+            var current = source;
+            for (int i = 0; i < dist; i++)
+            {
+                if (source.GetNW() != null)
+                {
+                    current = source.GetNW();
+                    tiles.Add(current);
+                }
+            }
+            return tiles;
+        }
+    }
+}
